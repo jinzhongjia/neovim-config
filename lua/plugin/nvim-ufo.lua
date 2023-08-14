bb749b898f99e121ca26ef8741536f1f3912dcdd
jinzhongjia <mail@nvimer.org>
2023-08-14 10:21:55 +0800

    add nvim-ufo

---
+++ lua/plugin/nvim-ufo.lua
@@ -0,0 +1,18 @@
+local status, nvim_ufo = pcall(require, "ufo")
+if not status then
+	vim.notify("not found ufo")
+	return
+end
+
+nvim_ufo.setup({
+	provider_selector = function(bufnr, filetype, buftype)
+		return { "treesitter", "indent" }
+	end,
+})
+
+vim.keymap.set("n", "zR", function()
+	nvim_ufo.openAllFolds()
+end)
+vim.keymap.set("n", "zM", function()
+	nvim_ufo.closeAllFolds()
+end)
