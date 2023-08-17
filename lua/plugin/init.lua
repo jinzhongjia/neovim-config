e6790b5c3b2762dc86100df6d446b35d13bb9da2
jinzhongjia <mail@nvimer.org>
2023-08-17 21:30:52 +0800

    add a new theme, vscode theme

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -307,11 +307,22 @@ require("lazy").setup({
     },
     {
         "projekt0n/github-nvim-theme",
+        enabled = false,
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             require("github-theme").setup({})
             vim.cmd("colorscheme github_dark_dimmed")
+        end,
+    },
+    {
+        "Mofiqul/vscode.nvim",
+        enabled = true,
+        priority = 1000,
+        config = function()
+            local vscode = require("vscode")
+            vscode.setup()
+            vscode.load()
         end,
     },
 })
