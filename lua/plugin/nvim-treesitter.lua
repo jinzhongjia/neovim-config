a6ab0b4ab6975dd73601394b4a15dcbbee581ec1
jinzhongjia <mail@nvimer.org>
2023-08-23 16:57:28 +0800

    change Incremental selection

--- lua/plugin/nvim-treesitter.lua
+++ lua/plugin/nvim-treesitter.lua
@@ -56,13 +56,16 @@ treesitter.setup({
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+    indent = {
+        enable = true,
+    },
     incremental_selection = {
         enable = true,
         keymaps = {
-            init_selection = "gnn", -- set to `false` to disable one of the mappings
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
+            init_selection = "<CR>",
+            node_incremental = "<CR>",
+            node_decremental = "<BS>",
+            scope_incremental = "<TAB>",
         },
     },
     context_commentstring = {
