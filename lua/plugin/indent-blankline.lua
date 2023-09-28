9eb93b0ee3ad4be05b8d148881cea957560d6bbe
jinzhongjia <mail@nvimer.org>
2023-09-28 15:46:09 +0800

    updtae indent-blankline

--- lua/plugin/indent-blankline.lua
+++ lua/plugin/indent-blankline.lua
@@ -4,16 +4,4 @@ if not status then
     return
 end
 
-local highlight = {
-    "CursorColumn",
-    "Whitespace",
-}
-
-ibl.setup {
-    indent = { highlight = highlight, char = "" },
-    whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-    },
-    scope = { enabled = false },
-}
+ibl.setup()
