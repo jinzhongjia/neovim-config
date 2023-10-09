5fcd6bb3868e46862636599f502363b9cd15096e
jinzhongjia <mail@nvimer.org>
2023-10-09 17:23:25 +0800

    add vtsls for inlay_hint

---
+++ lua/plugin/mason/lsp/config/vtsls.lua
@@ -0,0 +1,44 @@
+local opt = {
+    settings = {
+        typescript = {
+            inlayHints = {
+                parameterTypes = {
+                    enabled = true,
+                },
+                variableTypes = {
+                    enabled = true,
+                },
+                propertyDeclarationTypes = {
+                    enabled = true,
+                },
+                functionLikeReturnTypes = {
+                    enabled = true,
+                },
+                enumMemberValues = {
+                    enabled = true,
+                },
+            },
+        },
+        javascript = {
+            inlayHints = {
+                parameterTypes = {
+                    enabled = true,
+                },
+                variableTypes = {
+                    enabled = true,
+                },
+                propertyDeclarationTypes = {
+                    enabled = true,
+                },
+                functionLikeReturnTypes = {
+                    enabled = true,
+                },
+                enumMemberValues = {
+                    enabled = true,
+                },
+            },
+        },
+    },
+}
+
+return opt
