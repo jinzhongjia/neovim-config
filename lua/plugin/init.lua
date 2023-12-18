63a026afb71cb93f307336930a9818bcd274301a
“âjinzhongjia <mail@nvimer.org>
2023-12-18 21:43:45 +0800

    add plugin neodim

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -387,6 +387,29 @@ require("lazy").setup({
         event = "VeryLazy",
         opts = {},
     },
+    {
+        "zbirenbaum/neodim",
+        event = "LspAttach",
+        config = function()
+            require("neodim").setup({
+                refresh_delay = 75,
+                alpha = 0.75,
+                blend_color = "#000000",
+                hide = {
+                    underline = true,
+                    virtual_text = true,
+                    signs = true,
+                },
+                regex = {
+                    "[uU]nused",
+                    "[nN]ever [rR]ead",
+                    "[nN]ot [rR]ead",
+                },
+                priority = 128,
+                disable = {},
+            })
+        end,
+    },
 
     -- unpack(require("theme").theme()),
 }, {
