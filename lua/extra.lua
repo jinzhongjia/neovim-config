284790f4e01a7aa16758a9ef5d4edcf73753d8bf
jinzhongjia <mail@nvimer.org>
2024-01-29 21:47:51 +0800

    增加windows端的neovide支持

--- lua/extra.lua
+++ lua/extra.lua
@@ -32,7 +32,7 @@ if vim.g.neovide then
 
     vim.g.neovide_cursor_antialiasing = true
 
-    vim.g.neovide_input_ime = false
+    vim.g.neovide_input_ime = true
 
     vim.g.neovide_cursor_animation_length = 0
 end
