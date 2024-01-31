c4dd49a3ebcddc636d21d6a46a16baa086a41f82
jinzhongjia <mail@nvimer.org>
2024-01-31 16:20:53 +0800

    add lspui signature support for lualine

--- lua/plugin/lualine.lua
+++ lua/plugin/lualine.lua
@@ -6,7 +6,11 @@ end
 
 vim.g.gitblame_display_virtual_text = 0
 
+local LspUI = require("LspUI")
 local git_blame = require("gitblame")
+
+local is_insert = false
+local is_blame = false
 
 lualine.setup({
     sections = {
@@ -23,13 +27,38 @@ lualine.setup({
         },
         lualine_c = {
             {
-                git_blame.get_current_blame_text,
+                function()
+                    if is_insert then
+                        local signature = LspUI.api.signature()
+                        if not signature then
+                            return ""
+                        end
+                        if not signature.hint then
+                            return signature.label
+                        end
+                        local res = ""
+                        for i, parameter in ipairs(signature.parameters) do
+                            res = string.format("%s%s%s", res, parameter, i == #signature.parameters and "" or ", ")
+                        end
+
+                        return res
+                    elseif is_blame then
+                        return git_blame.get_current_blame_text()
+                    end
+                end,
                 cond = function()
+                    local mode_info = vim.api.nvim_get_mode()
+                    local mode = mode_info["mode"]
+                    is_insert = mode:find("i") ~= nil or mode:find("ic") ~= nil
+
                     local text = git_blame.get_current_blame_text()
                     if text then
-                        return text ~= ""
+                        is_blame = text ~= ""
+                    else
+                        is_blame = false
                     end
-                    return false
+
+                    return is_insert or is_blame
                 end,
             },
         },
