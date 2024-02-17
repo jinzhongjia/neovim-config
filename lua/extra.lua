674a5e3944a0cae96119695c2a7f1d8d116f31d9
jinzhongjia <mail@nvimer.org>
2024-02-17 17:11:40 +0800

    update font setting

--- lua/extra.lua
+++ lua/extra.lua
@@ -3,16 +3,16 @@ if vim.g.neovide then
     local home = vim.fn.expand("~")
     vim.fn.chdir(home)
 
-    if vim.fn.has("win32") == 1 then
+    -- if vim.fn.has("win32") == 1 then
         vim.o.guifont = "Maple Mono SC NF:h15"
-    elseif vim.fn.has("linux") == 1 then
-        local xdg_session = os.getenv("XDG_SESSION_TYPE")
-        if xdg_session == "x11" then
-            vim.o.guifont = "Maple Mono NF:h11"
-        else
-            vim.o.guifont = "Maple Mono NF:h15"
-        end
-    end
+    -- elseif vim.fn.has("linux") == 1 then
+    --     local xdg_session = os.getenv("XDG_SESSION_TYPE")
+    --     if xdg_session == "x11" then
+    --         vim.o.guifont = "Maple Mono SC NF:h11"
+    --     else
+    --         vim.o.guifont = "Maple Mono SC NF:h15"
+    --     end
+    -- end
 
     vim.g.neovide_input_ime = false
     local function set_ime(args)
