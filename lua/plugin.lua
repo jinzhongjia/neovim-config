44a1134392b2f86f3824d0245b0d61a7fe2de8e3
jinzhongjia <mail@nvimer.org>
2024-05-22 20:43:45 +0800

    add `garbage-day`

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -478,6 +478,14 @@ require("lazy").setup({
             require("plugin.hlslens")
         end,
     },
+    {
+        "zeioth/garbage-day.nvim",
+        dependencies = "neovim/nvim-lspconfig",
+        event = "VeryLazy",
+        opts = {
+            -- your options here
+        },
+    },
 
     -- unpack(require("theme").theme()),
 }, {
