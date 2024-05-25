b333a7bc7ca247b61e5e5d86a5845f1309c5b037
jinzhongjia <mail@nvimer.org>
2024-05-25 14:56:29 +0800

    update alpha

--- lua/plugin/alpha.lua
+++ lua/plugin/alpha.lua
@@ -3,60 +3,83 @@ if not status then
     vim.notify("not found alpha")
     return
 end
+local api, fn, fs = vim.api, vim.fn, vim.fs
 
-local base_path = "~/code"
+--- @type function
+local generate_config
 
-local header = {
-    type = "text",
-    val = {
-        [[                                  __                   ]],
-        [[     ___     ___    ___   __  __ /\_\    ___ ___       ]],
-        [[    / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\     ]],
-        [[   /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \    ]],
-        [[   \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\   ]],
-        [[    \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/   ]],
-    },
-    opts = {
-        position = "center",
-        hl = "Type",
-        -- wrap = "overflow";
-    },
+-- basic path for code
+local project_path = "~/code"
+
+--- @alias alpha_config_position "left"|"center"|"right"
+
+--- @type alpha_config_position
+local header_position = "center"
+--- @type alpha_config_position
+local project_position = "center"
+--- @type alpha_config_position
+local button_position = "center"
+
+vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#DA4939" })
+vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#FF875F" })
+vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#FFC66D" })
+vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#00FF03" })
+vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#00AFFF" })
+vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#8800FF" })
+
+local header_content = {
+    " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+}
+local header_hl = {
+    "NeovimDashboardLogo1",
+    "NeovimDashboardLogo2",
+    "NeovimDashboardLogo3",
+    "NeovimDashboardLogo4",
+    "NeovimDashboardLogo5",
+    "NeovimDashboardLogo6",
 }
 
--- local function get_extension(fn)
---     local match = fn:match("^.+(%..+)$")
---     local ext = ""
---     if match ~= nil then
---         ext = match:sub(2)
---     end
---     return ext
--- end
---
--- local function icon(fn)
---     local nwd = require("nvim-web-devicons")
---     local ext = get_extension(fn)
---     return nwd.get_icon(fn, ext, { default = true })
--- end
+local function Makeheader()
+    local tbl = {}
+    for i, v in ipairs(header_content) do
+        tbl[i] = {
+            type = "text",
+            val = v,
+            opts = { hl = header_hl[i], shrink_margin = false, position = header_position },
+        }
+    end
+    return { type = "group", val = tbl }
+end
+
+local header = Makeheader()
 
 --- @param key string
 --- @param txt string
+--- @param position string
 --- @param callback function? optional
-local function button(key, txt, callback)
+--- @param retain boolean?
+local function button(key, txt, position, callback, retain)
     local sc_ = key:gsub("%s", ""):gsub("SPC", "<leader>")
 
     local opts = {
-        position = "center",
-        shortcut = key,
-        cursor = 3,
+        position = position,
+        shortcut = "[" .. key .. "] ",
+        cursor = position == "left" and 1 or 2,
         width = 50,
-        align_shortcut = "right",
+        align_shortcut = (position == "left" and "left" or "right"),
         hl_shortcut = "Keyword",
     }
 
     local function on_press()
         if callback then
-            vim.api.nvim_buf_delete(0, {})
-
+            if not retain then
+                vim.api.nvim_buf_delete(0, {})
+            end
             callback()
         end
     end
@@ -71,97 +94,114 @@ local function button(key, txt, callback)
     }
 end
 
-local function projects()
-    local iterator = vim.fs.dir(base_path)
+--- @type integer
+local project_index = 0
 
-    local tbl = {}
-    local index = 0
+local project_length = 7
+
+local function projects()
+    local path = fn.expand(project_path)
+    local iterator = fs.dir(path)
+    local tbl, index = {}, 0
     while true do
         local filename, type = iterator()
         if not filename then
             break
         end
         if type == "directory" then
-            tbl[index] = button(tostring(index), "  " .. filename, function()
-                ---@diagnostic disable-next-line: param-type-mismatch
-                vim.api.nvim_set_current_dir(vim.fn.fnamemodify(string.format("%s/%s", base_path, filename), ":p"))
-            end)
+            -- if index >= project_index and index < project_length + project_index then
+            tbl[index - project_index] = button(
+                tostring(index),
+                string.format(" %s", filename),
+                project_position,
+                function()
+                    local path1 = string.format("%s/%s", project_path, filename)
+                    local path2 = fn.expand(path1)
+                    api.nvim_set_current_dir(path2)
+                end
+            )
+            -- end
             index = index + 1
         end
     end
+    -- tbl[index] = button("d", "next 7 page", project_position, function()
+    --     if index % 7 == 0 then
+    --         project_index = project_index + project_length
+    --         alpha.redraw(generate_config())
+    --     end
+    -- end, true)
+    -- tbl[index + 1] = button("u", "up 7 page", project_position, function()
+    --     if 0 < index and index < project_length then
+    --         project_index = 0
+    --         alpha.redraw(generate_config())
+    --     else
+    --         project_index = project_index - project_length
+    --         alpha.redraw(generate_config())
+    --     end
+    -- end, true)
 
-    return {
-        type = "group",
-        val = tbl,
-        opts = {},
-    }
+    return { type = "group", val = tbl }
 end
 
-local section_project = {
-    type = "group",
-    val = {
-        {
-            type = "text",
-            val = "Projects",
-            opts = {
-                hl = "SpecialComment",
-                shrink_margin = false,
-                position = "center",
+generate_config = function()
+    local section_project = {
+        type = "group",
+        val = {
+            {
+                type = "text",
+                val = " Projects ",
+                opts = {
+                    hl = "SpecialComment",
+                    shrink_margin = false,
+                    position = project_position,
+                },
+            },
+            { type = "padding", val = 1 },
+            {
+                type = "group",
+                val = function()
+                    return { projects() }
+                end,
+                opts = { shrink_margin = false },
             },
         },
-        { type = "padding", val = 1 },
-        {
-            type = "group",
-            val = function()
-                return { projects() }
-            end,
-            opts = { shrink_margin = false },
+    }
+
+    local buttons = {
+        type = "group",
+        val = {
+            { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = button_position } },
+            { type = "padding", val = 1 },
+            button("e", " New file", button_position, function()
+                vim.cmd([[ene]])
+            end),
+            button("c", " Configuration", button_position, function()
+                local config_path = vim.fn.stdpath("config")
+                ---@diagnostic disable-next-line: param-type-mismatch
+                vim.fn.chdir(config_path)
+                vim.api.nvim_command("bdelete")
+            end),
+            button("u", " Update plugins", button_position, function()
+                vim.cmd([[Lazy sync]])
+            end),
         },
-    },
-}
+    }
 
-local buttons = {
-    type = "group",
-    val = {
-        { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
-        { type = "padding", val = 1 },
-        button("e", "  New file", function()
-            vim.cmd([[ene]])
-        end),
-        button("c", "  Configuration", function()
-            local config_path = vim.fn.stdpath("config")
-            ---@diagnostic disable-next-line: param-type-mismatch
-            vim.fn.chdir(config_path)
-            vim.api.nvim_command("bdelete")
-        end),
-        button("u", "  Update plugins", function()
-            vim.cmd([[Lazy sync]])
-        end),
-    },
-    position = "center",
-}
+    local theme = {
+        layout = {
+            { type = "padding", val = 2 },
+            header,
+            { type = "padding", val = 2 },
+            section_project,
+            { type = "padding", val = 2 },
+            buttons,
+        },
+        opts = {
+            margin = 5,
+        },
+    }
 
-local theme = {
-    layout = {
-        { type = "padding", val = 2 },
-        header,
-        { type = "padding", val = 2 },
-        section_project,
-        { type = "padding", val = 2 },
-        buttons,
-    },
-    opts = {
-        margin = 5,
-        setup = function()
-            vim.api.nvim_create_autocmd("DirChanged", {
-                pattern = "*",
-                group = "alpha_temp",
-                callback = function()
-                    require("alpha").redraw()
-                end,
-            })
-        end,
-    },
-}
+    return theme
+end
 
-alpha.setup(theme)
+alpha.setup(generate_config())
