31eb7b33d0e460c989022543b64b752236efdbe6
jinzhongjia <mail@nvimer.org>
2024-05-28 20:51:52 +0800

    update hlslens config

--- lua/plugin/hlslens.lua
+++ lua/plugin/hlslens.lua
@@ -8,33 +8,23 @@ hlslens.setup({
     nearest_only = true,
 })
 
---- @param lhs string
---- @param fn function
-local function key_map_fn(lhs, fn)
-    vim.api.nvim_set_keymap("n", lhs, "", {
-        noremap = true,
-        silent = true,
-        callback = function()
-            vim.api.nvim_exec2(fn(), {})
-            hlslens.start()
-        end,
-    })
-end
+local kopts = { noremap = true, silent = true }
 
-local function key_map(lhs, command)
-    key_map_fn(lhs, function()
-        return command
-    end)
-end
+vim.api.nvim_set_keymap(
+    "n",
+    "n",
+    [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+    kopts
+)
+vim.api.nvim_set_keymap(
+    "n",
+    "N",
+    [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+    kopts
+)
+vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 
-key_map_fn("n", function()
-    return string.format("normal! %dn", vim.v.count1)
-end)
-key_map_fn("N", function()
-    return string.format("normal! %dN", vim.v.count1)
-end)
-key_map("*", "*")
-key_map("#", "#")
-key_map("g*", "g*")
-key_map("g#", "g#")
-vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>noh<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
