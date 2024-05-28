cc578a2e301a853e0c2297fdc45c4a990482f026
jinzhongjia <mail@nvimer.org>
2024-05-28 14:09:35 +0800

    update theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -149,6 +149,19 @@ M.theme = function()
                 -- vim.cmd[[colorscheme nano-theme]]
             end,
         },
+        {
+            "olivercederborg/poimandres.nvim",
+            lazy = false,
+            priority = 1000,
+            config = function()
+                require("poimandres").setup({})
+            end,
+
+            -- optionally set the colorscheme within lazy config
+            init = function()
+                vim.cmd("colorscheme poimandres")
+            end,
+        },
     }
 end
 
