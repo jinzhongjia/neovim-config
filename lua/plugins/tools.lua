5c9a8e499c51c1fd0244e2c38774445b6dc04c5e
jinzhongjia <mail@nvimer.org>
2025-01-27 00:59:09 +0800

    update

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -63,4 +63,15 @@ return
         "itchyny/vim-highlighturl",
         event = "VeryLazy",
     },
+    {
+        "chrishrb/gx.nvim",
+        keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+        cmd = { "Browse" },
+        init = function()
+            vim.g.netrw_nogx = 1 -- disable netrw gx
+        end,
+        dependencies = { "nvim-lua/plenary.nvim" }, -- Required for Neovim < 0.10.0
+        config = true, -- default settings
+        submodules = false, -- not needed, submodules are required only for tests
+    },
 }
