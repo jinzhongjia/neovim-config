aebbebb21290e2e4f01553ce5bc8c795bcc8b029
jinzhongjia <mail@nvimer.org>
2025-01-26 14:48:19 +0800

    file-expolor

---
+++ lua/plugins/file-explorer.lua
@@ -0,0 +1,40 @@
+-- manager lazy.nvim self
+return
+--- @type LazySpec
+{
+    "nvim-tree/nvim-tree.lua",
+    event = "UIEnter",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        disable_netrw = true,
+        sync_root_with_cwd = true,
+        filters = {
+            dotfiles = true,
+            custom = { "node_modules" },
+        },
+        actions = {
+            open_file = {
+                resize_window = true,
+                quit_on_open = true,
+            },
+        },
+        -- keymap override
+        on_attach = function(buffer_id)
+            local api = require("nvim-tree.api")
+
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = buffer_id, noremap = true, silent = true, nowait = true }
+            end
+            -- use add default mappings
+            api.config.mappings.default_on_attach(buffer_id)
+
+            vim.keymap.set("n", "sv", api.node.open.vertical, opts("Open: Vertical Split"))
+            vim.keymap.set("n", "sh", api.node.open.horizontal, opts("Open: Horizontal Split"))
+        end,
+    },
+    keys = {
+        { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
+    },
+}
