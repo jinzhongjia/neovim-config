9384a551c0c0854e4440444ea3b8aea8bdca8942
jinzhongjia <mail@nvimer.org>
2025-01-30 08:59:51 +0800

    add treesj

--- lua/plugins/edit/init.lua
+++ lua/plugins/edit/init.lua
@@ -40,4 +40,10 @@ return
             })
         end,
     },
+    {
+        "Wansmer/treesj",
+        keys = { "<space>m", "<space>j", "<space>s" },
+        dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+        opts = {},
+    },
 }
