c7edf78a8da36987cbabd9d654b4c46d0ee77d42
jinzhongjia <mail@nvimer.org>
2025-01-31 08:47:04 +0800

    add github support

---
+++ lua/plugins/github.lua
@@ -0,0 +1,12 @@
+return
+--- @type LazySpec
+{
+    "pwntester/octo.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+}
