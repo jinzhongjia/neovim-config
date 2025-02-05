5277867b4ea552ad5be2a724b096e86be0ec1ad5
jinzhongjia <mail@nvimer.org>
2025-02-05 23:13:59 +0800

    disable auto reload

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -28,6 +28,11 @@ require("lazy").setup({
         enabled = true,
         notify = false,
     },
+    change_detection = {
+        -- automatically check for config file changes and reload the ui
+        enabled = false,
+        notify = false, -- get a notification when changes are found
+    },
     install = {
         colorscheme = { "arctic" },
     },
