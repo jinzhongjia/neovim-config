395a8f34db073c82b577318ac5ff11da9803bc53
jinzhongjia <mail@nvimer.org>
2025-02-11 09:41:21 +0800

    update little config

--- lua/plugins/start.lua
+++ lua/plugins/start.lua
@@ -3,6 +3,7 @@ return
 {
     "goolord/alpha-nvim",
     dependencies = "echasnovski/mini.icons",
+    event = "VIMEnter",
     config = function()
         require("alpha").setup(require("alpha.themes.startify").config)
     end,
