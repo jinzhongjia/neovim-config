6585e550b4220d9428ccb4cbd0e30ea7fc257d09
jinzhongjia <mail@nvimer.org>
2025-02-15 23:09:35 +0800

    remove lazygit.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,27 +15,6 @@ return
         },
     },
     {
-        "kdheepak/lazygit.nvim",
-        event = "VeryLazy",
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        -- setting the keybinding for LazyGit with 'keys' is recommended in
-        -- order to load the plugin when the command is run for the first time
-        keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-        },
-    },
-
-    {
         "NeogitOrg/neogit",
         dependencies = {
             "nvim-lua/plenary.nvim", -- required
