2ede562c8aadca5c2ca365b80f9685a2e5fa4bd2
jinzhongjia <mail@nvimer.org>
2025-03-26 00:07:57 +0800

    add telescope dap nad live grep args

--- lua/plugins/edit/search.lua
+++ lua/plugins/edit/search.lua
@@ -156,6 +156,29 @@ return
         end,
     },
     {
+        "nvim-telescope/telescope-dap.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "mfussenegger/nvim-dap",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        event = "VeryLazy",
+        config = function()
+            require("telescope").load_extension("dap")
+        end,
+    },
+    {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        branch = "master",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+            require("telescope").load_extension("live_grep_args")
+        end,
+    },
+    {
         "kevinhwang91/nvim-hlslens",
         dependencies = "kevinhwang91/nvim-ufo",
         event = "VeryLazy",
