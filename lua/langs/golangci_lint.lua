cd4f70a045346bd0e8ff7fad84ef80bfa6305ed1
jinzhongjia <mail@nvimer.org>
2025-03-26 09:47:06 +0800

    update golangci_lint_ls config

--- lua/langs/golangci_lint.lua
+++ lua/langs/golangci_lint.lua
@@ -2,9 +2,20 @@ return
 --- @type LangSpec
 {
     lsp = "golangci_lint_ls",
-    opt = {},
+    opt = {
+        init_options = {
+            command = {
+                "golangci-lint",
+                "run",
+                "--output.json.path",
+                "stdout",
+                "--show-stats=false",
+                "--issues-exit-code=1",
+            },
+        },
+    },
     lint = {},
-    others = {"golangci-lint"},
+    others = { "golangci-lint" },
     before_set = nil,
     after_set = nil,
     plugins = {},
