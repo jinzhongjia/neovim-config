6654d1fe29f50cf93ab456bcc0b44f9bc3da760b
jinzhongjia <mail@nvimer.org>
2025-03-31 08:10:57 +0800

    update debug config

--- lua/plugins/edit/debug.lua
+++ lua/plugins/edit/debug.lua
@@ -55,6 +55,33 @@ return
             { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
             { "<leader>dn", function () require('persistent-breakpoints.api').clear_all_breakpoints() end , desc = "Clear All BreakPoint"}
         },
+        init = function()
+            vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
+            vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+            vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+
+            --
+            vim.fn.sign_define(
+                "DapBreakpoint",
+                { text = "󰄯", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+            )
+            vim.fn.sign_define(
+                "DapBreakpointCondition",
+                { text = "󰟃", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+            )
+            vim.fn.sign_define(
+                "DapBreakpointRejected",
+                { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+            )
+            vim.fn.sign_define(
+                "DapLogPoint",
+                { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+            )
+            vim.fn.sign_define(
+                "DapStopped",
+                { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+            )
+        end,
     },
     {
         "rcarriga/nvim-dap-ui",
@@ -114,35 +141,6 @@ return
             "nvim-treesitter/nvim-treesitter",
             "mfussenegger/nvim-dap",
         },
-        config = function()
-            local vt = require("nvim-dap-virtual-text")
-            ---@diagnostic disable-next-line: missing-fields
-            vt.setup({ commented = true })
-
-            vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
-            vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
-            vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
-
-            vim.fn.sign_define(
-                "DapBreakpoint",
-                { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-            )
-            -- vim.fn.sign_define(
-            --     "DapBreakpointCondition",
-            --     { text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-            -- )
-            -- vim.fn.sign_define(
-            --     "DapBreakpointRejected",
-            --     { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-            -- )
-            -- vim.fn.sign_define(
-            --     "DapLogPoint",
-            --     { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
-            -- )
-            -- vim.fn.sign_define(
-            --     "DapStopped",
-            --     { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
-            -- )
-        end,
+        opts = { commented = true },
     },
 }
