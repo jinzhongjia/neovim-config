c0a36df6ee56faf3ad02226590b458a3db2b8139
jinzhongjia <jinzhongjia@butterfly.tech>
2025-04-04 23:48:41 +0800

    add mini pairs

--- lua/plugins/edit/debug.lua
+++ lua/plugins/edit/debug.lua
@@ -109,11 +109,11 @@ return
                         size = 0.3, -- 40 columns
                         position = "left",
                     },
-                    -- {
-                    --     elements = { "repl" },
-                    --     size = 0.25, -- 25% of total lines
-                    --     position = "bottom",
-                    -- },
+                    {
+                        elements = { "repl" },
+                        size = 0.25, -- 25% of total lines
+                        position = "bottom",
+                    },
                 },
                 floating = {
                     max_height = nil, -- These can be integers or a float between 0 and 1.
