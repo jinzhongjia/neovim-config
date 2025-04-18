8ff71684eb9651f9891b505f86099f1dabe5faad
jinzhongjia <mail@nvimer.org>
2025-04-19 01:23:41 +0800

    remove yanky

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -73,35 +73,6 @@ return
         submodules = false, -- not needed, submodules are required only for tests
     },
     {
-        "gbprod/yanky.nvim",
-        event = "VeryLazy",
-        opts = {},
-        keys = {
-            {
-                "<leader>p",
-                function()
-                    require("telescope").extensions.yank_history.yank_history({})
-                end,
-                desc = "Open Yank History",
-            },
-            { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
-            { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
-            { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
-            { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
-            { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
-            { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
-            { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
-            { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
-            { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
-            { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
-            { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
-            { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
-            { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
-            { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
-            { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
-        },
-    },
-    {
         "stevearc/quicker.nvim",
         event = "FileType qf",
         ---@module "quicker"
