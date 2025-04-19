ddf49cbe703fc12c9edcd6f46987a656588d1659
jinzhongjia <mail@nvimer.org>
2025-04-19 21:17:54 +0800

    update codecompanion setting

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -207,6 +207,12 @@ return
                             },
                             codebase = require("vectorcode.integrations").codecompanion.chat.make_slash_command(),
                         },
+                        tools = {
+                            vectorcode = {
+                                description = "Run VectorCode to retrieve the project context.",
+                                callback = require("vectorcode.integrations").codecompanion.chat.make_tool(),
+                            },
+                        },
                     },
                     inline = { adapter = "copilot" },
                 },
@@ -235,5 +241,6 @@ return
         "Davidyz/VectorCode",
         dependencies = { "nvim-lua/plenary.nvim" },
         cmd = "VectorCode", -- if you're lazy-loading VectorCode
+        opts = {},
     },
 }
