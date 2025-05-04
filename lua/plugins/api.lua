d9da70401eafd64b5abbe0cfb9e63bc24cb3744e
jinzhongjia <mail@nvimer.org>
2025-05-04 09:32:31 +0800

    update neovim config

--- lua/plugins/api.lua
+++ lua/plugins/api.lua
@@ -3,8 +3,17 @@ return
 {
     {
         "rest-nvim/rest.nvim",
+        event = "VeryLazy",
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
+        },
+    },
+    {
+        "mistweaverco/kulala.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            global_keymaps = false,
         },
     },
 }
