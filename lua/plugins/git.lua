413d6afe5191652f814edfe5704913a74b280592
jinzhongjia <mail@nvimer.org>
2025-05-05 20:23:35 +0800

    feat(plugin): add plugin gitgraph.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -54,4 +54,40 @@ return
         version = "*",
         config = true,
     },
+    {
+        "isakbm/gitgraph.nvim",
+        dependencies = { "sindrets/diffview.nvim" },
+        ---@type I.GGConfig
+        opts = {
+            symbols = {
+                merge_commit = "M",
+                commit = "*",
+            },
+            format = {
+                timestamp = "%H:%M:%S %d-%m-%Y",
+                fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+            },
+            hooks = {
+                -- Check diff of a commit
+                on_select_commit = function(commit)
+                    vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+                    vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+                end,
+                -- Check diff from commit a -> commit b
+                on_select_range_commit = function(from, to)
+                    vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+                    vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+                end,
+            },
+        },
+        keys = {
+            {
+                "<leader>gl",
+                function()
+                    require("gitgraph").draw({}, { all = true, max_count = 5000 })
+                end,
+                desc = "GitGraph - Draw",
+            },
+        },
+    },
 }
