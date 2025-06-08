f11401cf4cbd5cbca68e02662e2482328574c751
jinzhongjia <mail@nvimer.org>
2025-06-08 15:14:45 +0800

    feat(plugins): 新增 todo 插件配置
    - 添加 bngarren/checkmate.nvim 插件
    - 支持 markdown 文件懒加载
    - 预留插件配置项

---
+++ lua/plugins/todo.lua
@@ -0,0 +1,13 @@
+return
+--- @type LazySpec
+{
+    {
+        "bngarren/checkmate.nvim",
+        ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
+        event = "VeryLazy",
+        opts = {
+            -- your configuration here
+            -- or leave empty to use defaults
+        },
+    },
+}
