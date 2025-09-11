a7fffe350e4fe845aec0352be24451a9116b918d
jinzhongjia <mail@nvimer.org>
2025-09-11 16:38:16 +0800

    refactor(db): remove SQL-related configurations
    - delete standalone SQL language configuration
    - remove nvim-dbee and sqls.nvim plugins
    - simplify database plugin configurations

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -23,24 +23,4 @@ return
             vim.g.db_ui_use_nerd_fonts = 1
         end,
     },
-    {
-        "kndndrj/nvim-dbee",
-        event = "VeryLazy",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-        },
-        build = function()
-            -- Install tries to automatically detect the install method.
-            -- if it fails, try calling it with one of these parameters:
-            --    "curl", "wget", "bitsadmin", "go"
-            require("dbee").install("go")
-        end,
-        config = function()
-            require("dbee").setup(--[[optional config]])
-        end,
-    },
-    {
-        "nanotee/sqls.nvim",
-        event = "VeryLazy",
-    },
 }
