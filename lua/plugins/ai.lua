1774f1040254e99170245a5e516b992f27a47c95
jinzhongjia <mail@nvimer.org>
2025-12-02 11:30:10 +0800

    chore(ai): refactor img-clip configuration
    - Move prompt_for_file_name to default options
    - Update image template format with markdown syntax
    - Add keyboard shortcut for paste image

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -731,13 +731,19 @@ return {
         "HakonHarnes/img-clip.nvim",
         event = "VeryLazy",
         opts = {
+            default = {
+                prompt_for_file_name = false,
+            },
             filetypes = {
                 codecompanion = {
-                    prompt_for_file_name = false,
-                    template = "[Image]($FILE_PATH)",
+                    url_encode_path = true,
+                    template = "![$CURSOR]($FILE_PATH)",
                     use_absolute_path = true,
                 },
             },
+        },
+        keys = {
+            { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from clipboard" },
         },
     },
 }
