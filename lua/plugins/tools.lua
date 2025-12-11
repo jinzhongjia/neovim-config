58294957f8d55d8769c3fe15ac42578a897c37ed
jinzhongjia <mail@nvimer.org>
2025-12-11 11:27:20 +0800

    chore(snacks): remove unused pickers and toggles
    - Remove nvim-early-retirement plugin
    - Remove picker keybindings for help, commands, icons
    - Remove LSP picker keybindings section
    - Remove toggle option mappings

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -23,11 +23,6 @@ return
         cmd = { "Translate", "TranslateW", "TranslateR", "TranslateX" },
     },
     {
-        "chrisgrieser/nvim-early-retirement",
-        event = { "BufReadPost", "BufNewFile" }, -- 需要监控 buffer 活动
-        config = true,
-    },
-    {
         "folke/todo-comments.nvim",
         event = { "BufReadPost", "BufNewFile" }, -- 打开文件时高亮 TODO
         dependencies = { "nvim-lua/plenary.nvim" },
@@ -575,25 +570,11 @@ return
                 desc = "Lines (buffer)",
             },
             {
-                "<leader>oh",
-                function()
-                    Snacks.picker.help()
-                end,
-                desc = "Help tags",
-            },
-            {
                 "<leader>ok",
                 function()
                     Snacks.picker.keymaps()
                 end,
                 desc = "Keymaps",
-            },
-            {
-                "<leader>oc",
-                function()
-                    Snacks.picker.commands()
-                end,
-                desc = "Commands",
             },
             {
                 "<leader>oC",
@@ -672,13 +653,6 @@ return
                 end,
                 desc = "Undo history",
             },
-            {
-                "<leader>oi",
-                function()
-                    Snacks.picker.icons()
-                end,
-                desc = "Icons",
-            },
 
             -- ===== Picker: 搜索内容 (leader-/ = search) =====
             {
@@ -711,44 +685,6 @@ return
                 end,
                 desc = "Resume search",
             },
-
-            -- ===== Picker: LSP =====
-            {
-                "gd",
-                function()
-                    Snacks.picker.lsp_definitions()
-                end,
-                desc = "Goto Definition",
-            },
-            {
-                "gD",
-                function()
-                    Snacks.picker.lsp_declarations()
-                end,
-                desc = "Goto Declaration",
-            },
-            {
-                "gr",
-                function()
-                    Snacks.picker.lsp_references()
-                end,
-                nowait = true,
-                desc = "References",
-            },
-            {
-                "gI",
-                function()
-                    Snacks.picker.lsp_implementations()
-                end,
-                desc = "Goto Implementation",
-            },
-            {
-                "gy",
-                function()
-                    Snacks.picker.lsp_type_definitions()
-                end,
-                desc = "Goto Type Definition",
-            },
         },
 
         init = function()
@@ -772,12 +708,6 @@ return
                         vim.print = _G.dd
                     end
 
-                    -- Create toggle mappings
-                    Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-                    Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-                    Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-                    Snacks.toggle.diagnostics():map("<leader>ud")
-                    Snacks.toggle.line_number():map("<leader>ul")
                     Snacks.toggle
                         .option("conceallevel", {
                             off = 0,
@@ -792,7 +722,6 @@ return
                             name = "Dark Background",
                         })
                         :map("<leader>ub")
-                    Snacks.toggle.inlay_hints():map("<leader>uh")
                     Snacks.toggle.dim():map("<leader>uD")
                     -- scroll 已禁用,移除相关 toggle
                 end,
