ee8e81bd57e22a9aec504f04ddedb3c7055c8630
jinzhongjia <mail@nvimer.org>
2025-12-23 16:18:28 +0800

    chore(neorg): disable lazy loading

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -3,7 +3,7 @@ return
 {
     {
         "nvim-neorg/neorg",
-        lazy = true, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
         version = "*", -- Pin Neorg to the latest stable release
         config = true,
     },
