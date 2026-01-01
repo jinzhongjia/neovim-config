fe636e7c25334ded5a2334e7aaab8c568ea0a181
jinzhongjia <mail@nvimer.org>
2026-01-01 22:46:18 +0800

    feat(floaterm): set width and height settings

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -4,6 +4,10 @@ return
     {
         "voldikss/vim-floaterm",
         event = "VeryLazy",
+        init = function()
+            vim.g.floaterm_width = 0.85
+            vim.g.floaterm_height = 0.8
+        end,
         keys = {
             { "<leader>ft", "<CMD>FloatermNew<CR>", mode = { "n", "t" }, desc = "floaterm new" },
             { "<leader>fj", "<CMD>FloatermPrev<CR>", mode = { "n", "t" }, desc = "floaterm prev" },
