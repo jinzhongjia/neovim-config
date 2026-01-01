0fc3be08b26d68f3786e9e95cc73a3abb4ea27ae
jinzhongjia <mail@nvimer.org>
2026-01-02 00:02:16 +0800

    feat(flash): add flash.nvim plugin
    Configure folke/flash.nvim with rainbow labels and define keymaps for
    jumping, treesitter search, and remote actions.

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,25 @@
+return
+--- @type LazySpec
+{
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {
+            label = {
+                rainbow = { enabled = true }, -- 彩虹标签
+            },
+            modes = {
+                char = {
+                    jump_labels = true, -- f/F/t/T 显示跳转标签
+                },
+            },
+        },
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+    },
+}
