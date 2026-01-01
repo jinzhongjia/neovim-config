26c45830b4f0829bca4d8e84e0af7493927c47de
jinzhongjia <mail@nvimer.org>
2026-01-02 00:10:03 +0800

    feat(treesj): add treesj plugin

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,16 @@
+return
+--- @type LazySpec
+{
+    {
+        "Wansmer/treesj",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {
+            use_default_keymaps = false,
+            max_join_length = 120,
+        },
+        keys = {
+            { "<leader>m", function() require("treesj").toggle() end, desc = "Toggle split/join" },
+        },
+    },
+}
+
