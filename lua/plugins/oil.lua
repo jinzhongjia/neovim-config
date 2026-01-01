151b4c8bb64eb45d4e0b341f7822a64b7b870f55
jinzhongjia <mail@nvimer.org>
2026-01-01 21:47:26 +0800

    feat(plugins): add oil lsp and git status plugins
    Add JezerM/oil-lsp-diagnostics.nvim and refractalize/oil-git-status.nvim
    to lua/plugins/oil.lua. Both plugins are disabled.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -18,4 +18,18 @@ return
             default_file_explorer = true,
         },
     },
+    {
+        "JezerM/oil-lsp-diagnostics.nvim",
+        enabled = false, -- oil.nvim 已禁用
+        dependencies = { "stevearc/oil.nvim" },
+        opts = {},
+    },
+    {
+        "refractalize/oil-git-status.nvim",
+        enabled = false, -- oil.nvim 已禁用
+        dependencies = { "stevearc/oil.nvim" },
+        opts = {
+            show_ignored = true,
+        },
+    },
 }
