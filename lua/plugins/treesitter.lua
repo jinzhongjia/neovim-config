b6007123daa5d9e0ad6de1ffae524c97f5a41b64
jinzhongjia <mail@nvimer.org>
2026-01-01 20:46:24 +0800

    feat(plugins): add nvim-treesitter and related plugins for enhanced syntax highlighting and context awareness
    Introduce the nvim-treesitter plugin along with nvim-treesitter-context and nvim-ts-autotag for improved syntax highlighting, context navigation, and automatic tag handling in Neovim. The configuration supports a wide range of languages and includes options for auto-installation and incremental selection, enhancing the overall coding experience.

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,122 @@
+return
+--- @type LazySpec
+{
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false,
+        event = { "VeryLazy" },
+        build = ":TSUpdate",
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        opts = {
+            ensure_installed = {
+                "c",
+                "go",
+                "lua",
+                "vim",
+                "vimdoc",
+                "bash",
+                "c_sharp",
+                "cmake",
+                "cpp",
+                "comment",
+                "css",
+                "diff",
+                "dockerfile",
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "gitignore",
+                "gomod",
+                "gosum",
+                "gowork",
+                "hjson",
+                "html",
+                "http",
+                "ini",
+                "javascript",
+                "json",
+                "json5",
+                "jsdoc",
+                "jsonc",
+                "luadoc",
+                "luap",
+                "make",
+                "markdown",
+                "meson",
+                "ninja",
+                "nix",
+                "proto",
+                "python",
+                "pug",
+                "regex",
+                "rust",
+                "scss",
+                "sql",
+                "svelte",
+                "toml",
+                "tsx",
+                "typescript",
+                "vue",
+                "yaml",
+                "zig",
+                "prisma",
+            },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = false },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn", -- set to `false` to disable one of the mappings
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            },
+        },
+        ---@param opts TSConfig
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        event = { "VeryLazy" },
+        opts = {
+            multiline_threshold = 5,
+        },
+        keys = {
+            -- stylua: ignore
+            { "<leader>[c", function() require("treesitter-context").go_to_context(vim.v.count1) end, desc = "jumping to context(upwards)" },
+        },
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            -- 全局默认配置
+            enable_close = true, -- 自动关闭标签
+            enable_rename = true, -- 自动重命名配对的标签
+            enable_close_on_slash = false, -- 在输入 </ 时自动关闭
+        },
+        config = function(_, opts)
+            require("nvim-ts-autotag").setup({
+                opts = opts,
+                -- 可以针对特定文件类型进行配置覆盖
+                per_filetype = {
+                    -- 例如: ["html"] = { enable_close = false }
+                },
+                -- 如果需要支持额外的语言，可以添加别名
+                -- aliases = {
+                --     ["your_language"] = "html",
+                -- }
+            })
+        end,
+    },
+}
