cba6e412905aef26dcf91b4b17793e19c056f685
jinzhongjia <mail@nvimer.org>
2026-01-02 13:10:19 +0800

    feat(which-key): add plugin with helix preset
    Configure folke/which-key.nvim with the helix preset and map
    <leader>? to show buffer-local keymaps.

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,20 @@
+return
+--- @type LazySpec
+{
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            preset = "helix",
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps",
+            },
+        },
+    },
+}
