499edbd494701cdd827d9405ae7f3f236ec79356
jinzhongjia <mail@nvimer.org>
2026-01-02 10:52:06 +0800

    feat(lsp): add html server configuration and ensure installation

---
+++ after/lsp/html.lua
@@ -0,0 +1,14 @@
+-- html: HTML LSP
+return {
+    settings = {
+        html = {
+            format = {
+                enable = true,
+            },
+            hover = {
+                documentation = true,
+                references = true,
+            },
+        },
+    },
+}
