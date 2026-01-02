9b0df80a8bdad38eb055461db6e3e6248dfb1622
jinzhongjia <mail@nvimer.org>
2026-01-02 11:07:30 +0800

    feat(barbar): replace diagnostic icons

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -95,10 +95,10 @@ return
                 buffer_number = false,
                 button = "󰅖",
                 diagnostics = {
-                    [vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
-                    [vim.diagnostic.severity.WARN] = { enabled = true, icon = " " },
-                    [vim.diagnostic.severity.INFO] = { enabled = true, icon = " " },
-                    [vim.diagnostic.severity.HINT] = { enabled = true, icon = "󰌵 " },
+                    [vim.diagnostic.severity.ERROR] = { enabled = true, icon = "󰅚" },
+                    [vim.diagnostic.severity.WARN] = { enabled = true, icon = "" },
+                    [vim.diagnostic.severity.INFO] = { enabled = true, icon = "" },
+                    [vim.diagnostic.severity.HINT] = { enabled = true, icon = "" },
                 },
                 filetype = {
                     custom_colors = false, -- 使用 nvim-web-devicons 的原生颜色
