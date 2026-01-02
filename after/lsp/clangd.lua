8ee32aa9e01e7760de5f400606606b8fce142479
jinzhongjia <mail@nvimer.org>
2026-01-02 10:50:44 +0800

    feat(lsp): add clangd server configuration and ensure installation

---
+++ after/lsp/clangd.lua
@@ -0,0 +1,11 @@
+-- clangd: C/C++ LSP
+return {
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+    },
+}
