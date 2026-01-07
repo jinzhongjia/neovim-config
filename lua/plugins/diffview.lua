90c476bece481042005bed1754a38825640c1b67
jinzhongjia <mail@nvimer.org>
2026-01-07 11:03:40 +0800

    feat(diffview): add nvim-web-devicons dependency

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,6 @@
 return {
     "sindrets/diffview.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("diffview").setup({
             enhanced_diff_hl = true, -- 启用增强高亮，模拟 VSCode 风格
