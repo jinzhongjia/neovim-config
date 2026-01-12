1f8b499bc6d844cbceb40ad46721ab3cc8a851fe
jinzhongjia <mail@nvimer.org>
2026-01-12 19:22:48 +0800

    feat: add numb support

---
+++ lua/plugins/numb.lua
@@ -0,0 +1,15 @@
+return
+--- @type LazySpec
+{
+    {
+        "nacro90/numb.nvim",
+        event = "CmdlineEnter",
+        opts = {
+            show_numbers = true,
+            show_cursorline = true,
+            hide_relativenumbers = true,
+            number_only = false,
+            centered_peeking = true,
+        },
+    },
+}
