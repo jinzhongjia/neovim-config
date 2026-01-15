6cc53e7c18e25d68a9ef0f2d3cbbf7d490f93975
jinzhongjia <mail@nvimer.org>
2026-01-15 15:13:38 +0800

    chore(lsp): remove cmake_language_server configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -73,7 +73,6 @@ return
                     "golangci_lint_ls",
                     "markdown_oxide",
                     "zls",
-                    "cmake_language_server",
                 },
                 -- 您也可以设置为 automatic_enable = false，然后手动调用 vim.lsp.enable()
                 automatic_enable = true,
