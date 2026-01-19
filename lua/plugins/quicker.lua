90ec322cadb064cd9ec265ef218955139d2241c2
jinzhongjia <mail@nvimer.org>
2026-01-19 11:09:46 +0800

    feat: add quicker.nvim plugin for enhanced quickfix navigation

---
+++ lua/plugins/quicker.lua
@@ -0,0 +1,26 @@
+return
+--- @type LazySpec
+{
+    {
+        "stevearc/quicker.nvim",
+        ft = "qf",
+        opts = {
+            keys = {
+                {
+                    ">",
+                    function()
+                        require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+                    end,
+                    desc = "Expand quickfix context",
+                },
+                {
+                    "<",
+                    function()
+                        require("quicker").collapse()
+                    end,
+                    desc = "Collapse quickfix context",
+                },
+            },
+        },
+    },
+}
