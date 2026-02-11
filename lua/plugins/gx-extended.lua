06618ace17049c118c98b4835acbe48c9e8aa6ba
jinzhongjia <mail@nvimer.org>
2026-02-11 16:49:39 +0800

    feat: add gx-extended plugin for enhanced URL handling

---
+++ lua/plugins/gx-extended.lua
@@ -0,0 +1,12 @@
+return
+--- @type LazySpec
+{
+    {
+        "rmagatti/gx-extended.nvim",
+        keys = { "gx" },
+        opts = {
+            enable_npm_imports = true,
+            enable_github_file_line = true,
+        },
+    },
+}
